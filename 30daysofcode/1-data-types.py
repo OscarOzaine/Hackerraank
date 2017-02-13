8c4e73712426cce66988e335a8242a65ffd3f867
Oscar Ozaine <oscar.ozaine@gmail.com>
2017-02-12 19:34:48 -0800

    30daysofcode

---
+++ 30daysofcode/1-data-types.py
@@ -0,0 +1,14 @@
+#Day 1: Data Types
+#https://www.hackerrank.com/challenges/30-data-types
+
+one = 0
+two = 0.0
+three = ''
+
+one = int(raw_input().strip())
+two = float(raw_input().strip())
+three = str(raw_input().strip())
+
+print one + i
+print two + d
+print s + three
