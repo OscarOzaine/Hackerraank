e52ea98e52b2e525f95d64db558a397c0ded977c
Oscar Ozaine <oscar.ozaine@gmail.com>
2017-02-22 23:40:05 -0800

    ...

---
+++ week-of-code/big-sort-2.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+def quickSort(arr):
+    less = []
+    pivotList = []
+    more = []
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        for i in arr:
+            if i < pivot:
+                less.append(i)
+            elif i > pivot:
+                more.append(i)
+            else:
+                pivotList.append(i)
+        less = quickSort(less)
+        more = quickSort(more)
+        return less + pivotList + more
+
+n = int(raw_input().strip())
+unsorted = []
+unsorted_i = 0
+for unsorted_i in xrange(n):
+    unsorted_t = str(raw_input().strip())
+    unsorted.append(int(unsorted_t))
+
+arr = quickSort(unsorted)
+
+for i in arr:
+    print i
+    
