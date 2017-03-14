2049fef8d13855caa1ffd2962d292d7ff8e38b03
Oscar Ozaine <oscar.ozaine@gmail.com>
2017-03-13 21:39:44 -0700

    wtf

---
+++ 30daysofcode/27-testing.py
@@ -0,0 +1,16 @@
+print 5
+
+print "4 3"
+print "-1 0 1 2"
+
+print "3 1"
+print "-1 0 1"
+
+print "5 4"
+print "-1 0 1 2 3"
+
+print "6 1"
+print "-1 0 1 2 3 4"
+
+print "7 6"
+print "-1 0 1 2 3 4 5"
