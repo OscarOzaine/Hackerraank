3fb90eaff7cfe72cd509a68013933ca9b0945d4d
Oscar Ozaine <oscar.ozaine@gmail.com>
2017-03-21 20:57:31 -0700

    strassen
    oh oh

--- stanford-course/week-1/matrix-multiplication.py
+++ stanford-course/week-1/matrix-multiplication.py
@@ -1,3 +1,156 @@
+import time
+from math import ceil, log
+
+def print_matrix(matrix):
+  for line in matrix:
+    print (' '.join(map(str, line)))
+
+def iterative_multiplication(A, B, n):
+  result = []
+  for x in range(n):
+    row = []
+    for y in range(n):
+      product = 0
+      for p in range(len(A[x])):
+        product += A[x][p] * B[p][y]
+
+      row.append(product)
+
+    result.append(row)
+
+  return result
+
+
+def add_matrix(A, B):
+  n = len(A)
+  C = [[0 for j in xrange(0, n)] for i in xrange(0, n)]
+  for i in xrange(0, n):
+    for j in xrange(0, n):
+      C[i][j] = A[i][j] + B[i][j]
+
+  return C
+
+
+def subtract_matrix(A, B):
+  n = len(A)
+  C = [[0 for j in xrange(0, n)] for i in xrange(0, n)]
+  for i in xrange(0, n):
+    for j in xrange(0, n):
+      C[i][j] = A[i][j] - B[i][j]
+
+  return C
+
+def strassenR(A, B):
+  """
+  Implementation of the strassen algorithm, similar to
+  http://en.wikipedia.org/w/index.php?title=Strassen_algorithm&oldid=498910018#Source_code_of_the_Strassen_algorithm_in_C_language
+  """
+  n = len(A)
+
+  if n <= 2:
+    return iterative_multiplication(A, B, len(A))
+  else:
+    # initializing the new sub-matrices
+    new_size = n/2
+    a11 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    a12 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    a21 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    a22 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+
+    b11 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    b12 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    b21 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    b22 = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+
+    aResult = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+    bResult = [[0 for j in xrange(0, new_size)] for i in xrange(0, new_size)]
+
+    # dividing the matrices in 4 sub-matrices:
+    for i in xrange(0, new_size):
+      for j in xrange(0, new_size):
+        a11[i][j] = A[i][j]            # top left
+        a12[i][j] = A[i][j + new_size]    # top right
+        a21[i][j] = A[i + new_size][j]    # bottom left
+        a22[i][j] = A[i + new_size][j + new_size] # bottom right
+
+        b11[i][j] = B[i][j]            # top left
+        b12[i][j] = B[i][j + new_size]    # top right
+        b21[i][j] = B[i + new_size][j]    # bottom left
+        b22[i][j] = B[i + new_size][j + new_size] # bottom right
+
+    # Calculating p1 to p7:
+    aResult = add_matrix(a11, a22)
+    bResult = add_matrix(b11, b22)
+    p1 = strassenR(aResult, bResult) # p1 = (a11+a22) * (b11+b22)
+
+    aResult = add_matrix(a21, a22)      # a21 + a22
+    p2 = strassenR(aResult, b11)  # p2 = (a21+a22) * (b11)
+
+    bResult = subtract_matrix(b12, b22) # b12 - b22
+    p3 = strassenR(a11, bResult)  # p3 = (a11) * (b12 - b22)
+
+    bResult = subtract_matrix(b21, b11) # b21 - b11
+    p4 =strassenR(a22, bResult)   # p4 = (a22) * (b21 - b11)
+
+    aResult = add_matrix(a11, a12)      # a11 + a12
+    p5 = strassenR(aResult, b22)  # p5 = (a11+a12) * (b22)
+
+    aResult = subtract_matrix(a21, a11) # a21 - a11
+    bResult = add_matrix(b11, b12)      # b11 + b12
+    p6 = strassenR(aResult, bResult) # p6 = (a21-a11) * (b11+b12)
+
+    aResult = subtract_matrix(a12, a22) # a12 - a22
+    bResult = add_matrix(b21, b22)      # b21 + b22
+    p7 = strassenR(aResult, bResult) # p7 = (a12-a22) * (b21+b22)
+
+    # calculating c21, c21, c11 e c22:
+    c12 = add_matrix(p3, p5) # c12 = p3 + p5
+    c21 = add_matrix(p2, p4)  # c21 = p2 + p4
+
+    aResult = add_matrix(p1, p4) # p1 + p4
+    bResult = add_matrix(aResult, p7) # p1 + p4 + p7
+    c11 = subtract_matrix(bResult, p5) # c11 = p1 + p4 - p5 + p7
+
+    aResult = add_matrix(p1, p3) # p1 + p3
+    bResult = add_matrix(aResult, p6) # p1 + p3 + p6
+    c22 = subtract_matrix(bResult, p2) # c22 = p1 + p3 - p2 + p6
+
+    # Grouping the results obtained in a single matrix:
+    C = [[0 for j in xrange(0, n)] for i in xrange(0, n)]
+    for i in xrange(0, new_size):
+      for j in xrange(0, new_size):
+        C[i][j] = c11[i][j]
+        C[i][j + new_size] = c12[i][j]
+        C[i + new_size][j] = c21[i][j]
+        C[i + new_size][j + new_size] = c22[i][j]
+    return C
+
+def strassen(A, B):
+  assert type(A) == list and type(B) == list
+  assert len(A) == len(A[0]) == len(B) == len(B[0])
+
+  # Make the matrices bigger so that you can apply the strassen
+  # algorithm recursively without having to deal with odd
+  # matrix sizes
+  nextPowerOfTwo = lambda n: 2**int(ceil(log(n,2)))
+
+  n = len(A)
+  m = nextPowerOfTwo(n)
+  APrep = [[0 for i in xrange(m)] for j in xrange(m)]
+  BPrep = [[0 for i in xrange(m)] for j in xrange(m)]
+
+  for i in xrange(n):
+    for j in xrange(n):
+      APrep[i][j] = A[i][j]
+      BPrep[i][j] = B[i][j]
+
+  CPrep = strassenR(APrep, BPrep)
+  C = [[0 for i in xrange(n)] for j in xrange(n)]
+
+  for i in xrange(n):
+    for j in xrange(n):
+      C[i][j] = CPrep[i][j]
+  return C
 
 n = int(raw_input().strip())
 
@@ -12,21 +165,19 @@ for x in range(0, n):
   for y in range(0, n):
     B[x][y] = int(raw_input().strip())
 
+timenow = time.time()
+timeclock = time.clock()
 
-print A[0][0]
+print_matrix(iterative_multiplication(A, B, n))
 
-result = []
-for x in range(n):
+print time.time() - timenow
+print time.clock() - timeclock
+timenow = time.time()
+timeclock = time.clock()
 
-  row = []
-  for y in range(n):
-    product = 0
-    for p in range(len(A[x])):
-      product += A[x][p] * B[p][y]
+print 
+print_matrix(strassen(A, B))
 
-    row.append(product)
+print time.time() - timenow
+print time.clock() - timeclock
 
-  result.append(row)
-
-
-print result
