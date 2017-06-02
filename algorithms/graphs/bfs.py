a0d63c6ed40e0b85f991dfa860e7476c2edd8efc
OscarOzaine <oscar.ozaine@gmail.com>
2017-06-01 22:40:34 -0700

    hmm

--- algorithms/graphs/bfs.py
+++ algorithms/graphs/bfs.py
@@ -29,38 +29,45 @@ class SolveGraph:
 						queue.append(i)
 						visited[i] = True
 
-	def dfs(self, s):
-		visited = [False] * len(self.graph)
-		queue = []
-		queue.append(s)
-		visited[s] = True
-
-		while queue:
-
-			s = queue.pop(len(queue)-1)
-			print s,
-			
-			for i in self.graph[s]:
-				if i < len(visited):
-					if visited[i] == False:
-						queue.append(i)
-						visited[i] = True
-
 
 	def dfsRecursive(self, start, end, visited=set(), path=[]):
-		print start,
+		#print start,
+		visited.add(start)
 		if start == end:
 			return path + [start]
 
 		for i in self.graph[start]:
 
 			if i not in visited:
-				visited.add(i)
-				p = self.dfsRecursive(i, end, visited, path + [i])
+				
+				p = self.dfsRecursive(i, end, visited, path + [start])
 				if p:
 					return p
 
 		return ''
+
+	def dfsRecursiveAllPaths(self, start, end):
+
+
+		stack = [(start, [start])]
+
+		while stack:
+			(vertex, path) = stack.pop()
+			for i in self.graph[vertex] - set(path):
+				if i == goal:
+					yield path + [i]
+				else:
+					stack.append((i, path + [i]))
+
+	def dfs_paths(self, start, goal, path=None):
+		if path is None:
+			path = [start]
+		if start == goal:
+			yield path
+		for next in self.graph[start] - set(path):
+			yield self.dfs_paths(next, goal, path + [next])
+
+			
 
 
 
@@ -78,15 +85,21 @@ g.addEdge(6, 6)
 
 print g.graph
 print 'bfs'
-g.bfs(2)
+g.bfs(0)
 print 
 
-print 'dfs'
-g.dfs(2)
-print
-
 print 'dfsRecursive'
-path = g.dfsRecursive(2, 6)
+path = g.dfsRecursive(0, 4)
+print path
+print 
+
+print 'dfsRecursiveAllPaths'
+paths = g.dfsRecursiveAllPaths(0, 4)
+print path
+print 
+
+print 'dfs_paths'
+paths = g.dfs_paths(3, 4)
 print path
 print 
 
