947da27ab8e5449b2af7f07cc90593eff8a339f6
OscarOzaine <oscar.ozaine@gmail.com>
2017-06-13 23:20:37 -0700

    ..

---
+++ codefights/interview-practice/groupingDishes.py
@@ -0,0 +1,18 @@
+def groupingDishes(dishes):
+    dictionary = {}
+    for d in dishes:
+        for i in d[1:]:
+            if i not in dictionary:
+                dictionary[i] = [d[0]]
+            else:
+                dictionary[i].append(d[0])
+                
+    result = []
+    for d in sorted(dictionary):
+        if len(dictionary[d])> 1:
+            result.append([d] + sorted(dictionary[d]))
+            
+    return result
+    
+        
+
