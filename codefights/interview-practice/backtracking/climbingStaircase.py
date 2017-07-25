112a1d063d29698f6d1525d8a8ca578a8dc01026
OscarOzaine <oscar.ozaine@gmail.com>
2017-07-24 22:29:52 -0700

    climbit

--- codefights/interview-practice/backtracking/climbingStaircase.py
+++ codefights/interview-practice/backtracking/climbingStaircase.py
@@ -1,34 +1,27 @@
-
+path = [0]*100
+pp = []
 def climbingStaircase(n, k):
-    result = stairs(n, k)
-    print result
-    '''
-    for array in result:
-        print (''.join(str(x) for x in array ) )
-        
-    '''
-    
-    
-def stairs(n, k):
-    array = []
-    if n == 1:
-        array.append([1])
-    elif n == 2:
-        tempArray = stairs(n-1, k)
-        for temp in tempArray:
-            array.append([1]+ temp)
-            
-        array.append([2])
-    else:
-        tempArray = stairs(n-1, k)
-        for temp in tempArray:
-            array.append([1]+ temp)
+    ss = 0
+    idx = 0
 
-        tempArray = stairs(n-2, k)
-        for temp in tempArray:
-            array.append([2]+ temp)
-        
-            
-    return array
-
+    stairs(n, k, ss, idx)
+    return pp
     
+    
+def stairs(n, k, ss, idx):
+    if ss == n:
+        p = []
+        for i in range(0, idx):
+            p.append(path[i])
+            
+        pp.append(p)
+        return
+
+    if ss > n:
+        return
+    
+    if ss < n:
+        for i in range(1, k+1):
+            path[idx] = i
+            stairs(n, k, ss + i, idx + 1)
+            
